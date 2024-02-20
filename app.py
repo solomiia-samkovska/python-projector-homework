4f20cb85b261195f8d3bced3a04682c17290210e
Solomiia <solomiya.kachan@gmail.com>
2024-02-20 13:40:51 +0100

    Add division to calculator

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 def sum(a, b):
     return a + b
 
-def sub(a,b):
+def sub(a, b):
     return a - b
 
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
 def calc():
-    operation = input("Select operation. For addition enter '+', for subtraction enter '-': ")
-    if operation not in ["+", "-"]:
+    operation = input("Select operation:\n for Addition enter '+'\n for Subtraction enter '-'\n for Multiplication enter '*'\n for Division enter '/'")
+    
+    if operation not in ["+", "-", "*", "/"]:
         print("Invalid input")
         return
     
@@ -18,6 +25,10 @@ def calc():
             print(f"{a} + {b} = ", sum(a,b))
         case "-":
             print(f"{a} - {b} = ", sub(a,b))
+        case "*":
+            print(f"{a} * {b} = ", mult(a,b))
+        case "/":
+            print(f"{a} / {b} = ", div(a,b))
 
 calc()
 
